e9f49edb53d6d42577a234142d3f0891c03993e2
meminguner <meminguner@example.com>
2024-12-27 00:21:58 +0300

    base url değiştirildi

--- frontend/src/services/http-common.jsx
+++ frontend/src/services/http-common.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 export default axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://mekanbul-odev-deneme-1-backend.vercel.app/",
   headers: {
     Accept: "application/json",
     "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",

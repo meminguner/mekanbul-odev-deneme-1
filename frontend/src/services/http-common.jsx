434824011786ee29e75945e8e9a813c35ec1eb39
meminguner <meminguner@example.com>
2024-12-27 00:38:20 +0300

    /api yazdık base url sonuna

--- frontend/src/services/http-common.jsx
+++ frontend/src/services/http-common.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 export default axios.create({
-  baseURL: "https://mekanbul-odev-deneme-1-backend.vercel.app/",
+  baseURL: "https://mekanbul-odev-deneme-1-backend.vercel.app/api",
   headers: {
     Accept: "application/json",
     "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
